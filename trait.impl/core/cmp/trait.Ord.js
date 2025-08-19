97d395193e72afcc5190291b02be49f514a135fb
nixpulvis <nixpulvis@users.noreply.github.com>
2025-08-19 17:54:54 +0000

    deploy: 2a43188b0a930ab78903a30c1c89f80869dda1c2

---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["grapl",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"grapl/struct.Node.html\" title=\"struct grapl::Node\">Node</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[238]}
